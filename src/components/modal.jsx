6e5a85b645c15bbe521dc8ea29456d1aa9b038c6
Pavel <pdinamo1995@icloud.com>
2026-05-11 23:42:59 +0700

    stabilize global scrollbar gutter

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -23,23 +23,11 @@ export const modalSectionVariants = {
 export function useModalScrollLock() {
   useEffect(() => {
     const previousBodyOverflow = document.body.style.overflow;
-    const previousBodyPaddingRight = document.body.style.paddingRight;
-    const previousHtmlOverflow = document.documentElement.style.overflow;
-    const previousHtmlScrollbarGutter = document.documentElement.style.scrollbarGutter;
-    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
 
-    document.documentElement.style.scrollbarGutter = 'stable';
-    if (scrollbarWidth > 0) {
-      document.body.style.paddingRight = `${scrollbarWidth}px`;
-    }
     document.body.style.overflow = 'hidden';
-    document.documentElement.style.overflow = 'hidden';
 
     return () => {
       document.body.style.overflow = previousBodyOverflow;
-      document.body.style.paddingRight = previousBodyPaddingRight;
-      document.documentElement.style.overflow = previousHtmlOverflow;
-      document.documentElement.style.scrollbarGutter = previousHtmlScrollbarGutter;
     };
   }, []);
 }
